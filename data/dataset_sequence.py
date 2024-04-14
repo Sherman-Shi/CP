9605d0cbff5ea3b5fcde5dbc1215ae5691d816a1
Sherman Shi <shizp22@mails.tsinghua.edu.cn>
2024-04-14 16:32:19 +0800

    main fix

--- data/dataset_sequence.py
+++ data/dataset_sequence.py
@@ -16,6 +16,85 @@ from collections import namedtuple
 Batch = namedtuple('Batch', 'trajectories conditions')
 
 # dataset class 
+class RSADataset(torch.utils.data.Dataset):
+    def __init__(self, config):
+        super().__init__()
+        self.env = load_environment(config["dataset"]["env_name"])
+        self.horizon = config["dataset"]["horizon"]
+        self.max_episode_len = config["dataset"]["max_episode_len"]
+        self.max_n_episodes = config["dataset"]["max_n_episodes"]
+        self.termination_penalty = config["dataset"]["termination_penalty"]
+        self.normalizer_name = config["dataset"]["normalizer_name"]
+        self.use_padding = config["dataset"]["use_padding"]
+        self.known_obs_len = config["target"]["known_obs_len"]
+        self.target_len = config["target"]["target_len"]
+        itr = sequence_dataset(self.env)
+
+        fields = ReplayBuffer(self.max_n_episodes, self.max_episode_len, self.termination_penalty)
+        for i, episode in enumerate(itr):
+            fields.add_path(episode)
+        fields.finalize()
+
+        self.normalizer = DatasetNormalizer(fields, self.normalizer_name, path_lengths=fields['path_lengths'])
+        self.indices = self.make_indices(fields.path_lengths, self.horizon)
+        self.observation_dim = fields.observations.shape[-1]
+        self.action_dim = fields.actions.shape[-1]
+        self.fields = fields
+        self.n_episodes = fields.n_episodes
+        self.path_lengths = fields.path_lengths
+        self.normalize()
+
+        print(fields)
+
+    def normalize(self, keys=['observations', 'actions']):
+        '''
+            normalize fields that will be predicted by the diffusion model
+        '''
+        for key in keys:
+            array = self.fields[key].reshape(self.n_episodes*self.max_episode_len, -1)
+            normed = self.normalizer(array, key)
+            self.fields[f'normed_{key}'] = normed.reshape(self.n_episodes, self.max_episode_len, -1)
+    
+    def make_indices(self, path_lengths, horizon):
+        '''
+            makes indices for sampling from dataset;
+            each index maps to a datapoint
+        '''
+        indices = []
+        for i, path_length in enumerate(path_lengths):
+            max_start = min(path_length - 1, self.max_episode_len - horizon)
+            if not self.use_padding:
+                max_start = min(max_start, path_length - horizon)
+            for start in range(max_start):
+                end = start + horizon
+                indices.append((i, start, end))
+        indices = np.array(indices)
+        return indices
+    
+    def get_conditions(self, trajectories):
+        '''
+            condition on current trajectories for planning
+        '''
+        known_traj = trajectories[:self.known_obs_len]
+        target = trajectories[-self.target_len:]
+        return {"known_obs": known_traj, "target": target}
+    
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx, eps=1e-4):
+        path_ind, start, end = self.indices[idx]
+
+        observations = self.fields.normed_observations[path_ind, start:end]
+        actions = self.fields.normed_actions[path_ind, start:end]
+        trajectories = np.concatenate([actions, observations], axis=-1)
+        conditions = self.get_conditions(trajectories)
+
+        batch = Batch(trajectories, conditions)
+
+        return batch
+
+
 class DynamicDataset(torch.utils.data.Dataset):
     def __init__(self, config):
         super().__init__()
--- main_train.py
+++ main_train.py
@@ -7,10 +7,8 @@ import sys
 sys.path.append("/home/zhenpeng/桌面/brainstorm/OSG")
 from utils.load_model import load_model_from_config
 from evaluating.eval import evaluate_model
-from data.dataset_sequence import DynamicDataset
-from data.dataset_sequence import TargetDataset
+from data.dataset_sequence import RSADataset
 from models.diffusion import GaussianDiffusionModel
-from models.diffusion import UnconditionalGaussianDiffusionModel
 from training.trainer import Trainer
 
 
@@ -33,14 +31,10 @@ def main():
         initialize_wandb(config)
 
     #dynamic
-    dynamic_dataset = DynamicDataset(config)    
+    dynamic_dataset = RSADataset(config)    
     dynamic_model = GaussianDiffusionModel(dynamic_dataset, config)
     dynamic_trainer = Trainer(dynamic_dataset, dynamic_model, config, model_type="dynamic_diffusion")
 
-    #target 
-    target_dataset = TargetDataset(dynamic_dataset.normalizer, config)
-    target_model = UnconditionalGaussianDiffusionModel(target_dataset, config)
-    target_traner = Trainer(target_dataset, target_model, config, model_type="target_diffuson")
 
     n_epochs = int(config['training']['n_total_train_steps'] // config['training']['n_steps_per_epoch'])
 
@@ -48,9 +42,6 @@ def main():
     for i in range(n_epochs):
         if config['training']['train_dynamic']:
             dynamic_trainer.train(n_train_steps=config['training']['n_steps_per_epoch'], current_epoch=i)
-        if config['training']['train_target']:
-            target_traner.train(n_train_steps=config['training']['n_steps_per_epoch'], current_epoch=i)
-
-
+            
 if __name__ == "__main__":
     main()
